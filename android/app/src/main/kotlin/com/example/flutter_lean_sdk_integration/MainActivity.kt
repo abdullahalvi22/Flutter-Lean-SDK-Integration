04bd3eed06533f165dbd239035b8d30be58bbdcf
M.Abdullah Alvi <abdullah.alvi22@gmail.com>
2026-01-22 12:30:59 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_lean_sdk_integration/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_lean_sdk_integration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Lean SDK Integration',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Lean SDK Integration'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Connect to Bank Account'),
+            ElevatedButton(onPressed: () {}, child: Text('Connect Account')),
+          ],
+        ),
+      ),
+    );
+  }
+}
